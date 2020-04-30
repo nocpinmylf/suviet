c827b481ddd0f90350bda8c33cadca7846a2bacc
nocpinmylf <lemduc@hotmail.com>
2020-04-30 17:03:33 +0700

    update js

--- javascript.js
+++ javascript.js
@@ -7,31 +7,6 @@ collapseBTN.addEventListener('click', () => {
   else header.classList.remove('show');
 });
 
-// typing text 
-let i = 0;
-let quoteString = `Lật lại những trang sử các thời kỳ, mỗi người dân Việt, dù trong hay ngoài nước, luôn được khơi dậy tình yêu quê hương, niềm tự hào tổ quốc. Đó là thời nghìn năm Bắc thuộc với tinh thần chống Hán
-hóa, nhiều cuộc khởi nghĩa giành lại chủ quyền diễn ra mà trận chiến thắng Bạch Đằng là đỉnh cao. Đó
-là thời kỳ độc lập tự chủ, cha ông không những bảo vệ non sông mà còn mở mang bờ cõi, không những
-giữ gìn mà còn làm phong phú trường tồn bản sắc văn hóa Việt Nam. Với những chiến công hiển hách
-chống phương Bắc xâm lược, những anh hùng Thập đạo tướng quân Lê Hoàn, Lý Thường Kiệt, Trần
-Quốc Tuấn, Lê Lợi, Nguyễn Huệ... mãi mãi lưu danh...`;
-let speed = 50;
-
-
-
-
-
-// window.addEventListener('scroll', () => {
-//   if(isInViewport(document.getElementById('quote'))) typeWriter();
-// });
-
-
-// function isInViewport (elem) { // khi scroll xuong va thay duoc elem thi true
-//   var bounding = elem.getBoundingClientRect();
-//   return bounding.top <= (window.innerHeight || document.documentElement.clientHeight);
-// };
-
-
 // lazy loading ???
 const imgList = document.querySelectorAll('img');
 
@@ -47,12 +22,31 @@ const lazy = target => {
         observer.disconnect();
       }
     });
-
   });
   io.observe(target);
 };
 imgList.forEach(lazy);
 
+
+// typing text 
+let i = 0;
+let quoteString = `Lật lại những trang sử các thời kỳ, mỗi người dân Việt, dù trong hay ngoài nước, luôn được khơi dậy tình yêu quê hương, niềm tự hào tổ quốc. Đó là thời nghìn năm Bắc thuộc với tinh thần chống Hán
+hóa, nhiều cuộc khởi nghĩa giành lại chủ quyền diễn ra mà trận chiến thắng Bạch Đằng là đỉnh cao. Đó
+là thời kỳ độc lập tự chủ, cha ông không những bảo vệ non sông mà còn mở mang bờ cõi, không những
+giữ gìn mà còn làm phong phú trường tồn bản sắc văn hóa Việt Nam. Với những chiến công hiển hách
+chống phương Bắc xâm lược, những anh hùng Thập đạo tướng quân Lê Hoàn, Lý Thường Kiệt, Trần
+Quốc Tuấn, Lê Lợi, Nguyễn Huệ... mãi mãi lưu danh...`;
+let speed = 50;
+
+// window.addEventListener('scroll', () => {
+//   if(isInViewport(document.getElementById('quote'))) typeWriter();
+// });
+
+
+// function isInViewport (elem) { // khi scroll xuong va thay duoc elem thi true
+//   var bounding = elem.getBoundingClientRect();
+//   return bounding.top <= (window.innerHeight || document.documentElement.clientHeight);
+// };
 
 function typeWriter() {
   if (i < quoteString.length) {
@@ -61,5 +55,6 @@ function typeWriter() {
     setTimeout(typeWriter, speed);
   }
 }
+
 
 typeWriter();
